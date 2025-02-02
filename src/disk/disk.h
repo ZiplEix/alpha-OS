8480430dd8922b44e2c1bc1d08bf2806904c37bc
ZiplEix <ZiplEix@users.noreply.github.com>
2025-02-02 18:49:41 +0800

    feat: abstract disk driver

--- src/disk/disk.h
+++ src/disk/disk.h
@@ -1,6 +1,19 @@
 #ifndef DISK_H
 #define DISK_H
 
-int disk_read_sector(int lba, int total, void *buf);
+typedef unsigned int ALPHAOS_DISK_TYPE;
+
+// Represents a real physical hard disk
+#define ALPHAOS_DISK_TYPE_REAL 0
+
+// The representation of an hard disk
+struct disk {
+    ALPHAOS_DISK_TYPE type;
+    int sector_size;
+};
+
+void disk_search_and_init();
+struct disk *disk_get(int index);
+int disk_read_block(struct disk *idisk, unsigned int lba, int total, void *buf);
 
 #endif // DISK_H
--- src/kernel.c
+++ src/kernel.c
@@ -82,6 +82,9 @@ void kernel_main() {
     // inti the heap
     kheap_init();
 
+    // search and init disks
+    disk_search_and_init();
+
     // Init interrupt descriptor table
     idt_init();
 
@@ -93,9 +96,6 @@ void kernel_main() {
 
     // Enable paging
     enable_paging();
-
-    char buf[512];
-    disk_read_sector(0, 1, buf);
 
     // Enable the systeme interrupts
     enable_interrupts();
