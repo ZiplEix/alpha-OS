01be82a0ba47202a0c7cbce969267293d35a030a
ZiplEix <ZiplEix@users.noreply.github.com>
2025-02-02 18:35:26 +0800

    feat: reading from disk with the ATA controller

--- src/kernel.c
+++ src/kernel.c
@@ -6,6 +6,7 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 uint16_t *video_memory = 0;
 uint16_t terminal_row = 0;
@@ -86,20 +87,15 @@ void kernel_main() {
 
     // Setup oaging
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
-    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
-    char *ptr = kzalloc(4096);
-    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void *)0x1000, (uint32_t) ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITABLE);
+    // Switch to the kernel paging chunk
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
     // Enable paging
     enable_paging();
 
-    char *ptr2 = (char *)0x1000;
-    ptr2[0] = 'A';
-    ptr2[1] = 'B';
-    print(ptr2);
-
-    print(ptr);
+    char buf[512];
+    disk_read_sector(0, 1, buf);
 
     // Enable the systeme interrupts
     enable_interrupts();
