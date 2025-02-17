9a7d8bbe1f5f839bc716e5df995ff42e30db3d98
ZiplEix <ZiplEix@users.noreply.github.com>
2025-02-18 00:42:11 +0900

    feat: implement FAT16 fopen function

--- src/memory/memory.h
+++ src/memory/memory.h
@@ -5,5 +5,6 @@
 
 void *memset(void *ptr, int value, size_t size);
 int memcmp(const void *ptr1, const void *ptr2, size_t size);
+void *memcpy(void *dest, const void *src, size_t size);
 
 #endif // MEMORY_H
