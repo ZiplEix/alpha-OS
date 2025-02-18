5a90bc75adbaa8f209505ca4d8c8b0e3a6e59673
ZiplEix <ZiplEix@users.noreply.github.com>
2025-02-19 01:03:29 +0900

    feat: implement FAT16 fstat function

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -124,6 +124,7 @@ int fat16_resolve(struct disk *disk);
 void *fat16_open(struct disk *disk, struct path_part *path, FILE_MODE mode);
 int fat16_read(struct disk *disk, void *descriptor, uint32_t size, uint32_t nmemb, char *out_ptr);
 int fat16_seek(void *private, uint32_t offset, FILE_SEEK_MODE seek_mode);
+int fat16_stat(struct disk *disk, void *private, struct file_stat *stat);
 
 struct filesystem fat16_fs =
 {
@@ -131,6 +132,7 @@ struct filesystem fat16_fs =
     .open = fat16_open,
     .read = fat16_read,
     .seek = fat16_seek,
+    .stat = fat16_stat
 };
 
 struct filesystem *fat16_init()
@@ -602,6 +604,27 @@ void *fat16_open(struct disk *disk, struct path_part *path, FILE_MODE mode)
     return descriptor;
 }
 
+int fat16_stat(struct disk *disk, void *private, struct file_stat *stat)
+{
+    int res = 0;
+    struct fat_file_descriptor *descriptor = (struct fat_file_descriptor *) private;
+    struct fat_item *item = descriptor->item;
+    if (item->type != FAT_ITEM_TYPE_FILE) {
+        res = -EINVARG;
+        goto out;
+    }
+
+    struct fat_directory_item *ritem = item->item;
+    stat->filesize = ritem->file_size;
+    stat->flags = 0x00;
+
+    if (ritem->attributes & FAT_FILE_READ_ONLY) {
+        stat->flags |= FILE_STAT_READ_ONLY;
+    }
+
+out:
+    return res;
+}
 
 int fat16_read(struct disk *disk, void *descriptor, uint32_t size, uint32_t nmemb, char *out_ptr)
 {
