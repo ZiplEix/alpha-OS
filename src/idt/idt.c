232d2925d37c27faaa6a8d4e668794c92fce0616
ZiplEix <ZiplEix@users.noreply.github.com>
2025-03-04 00:37:10 +0900

    feat: create the ability to create and execute kernel commands

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -8,9 +8,11 @@
 struct idt_desc idt_descriptors[ALPHAOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
+static ISR80H_COMMAND isr80h_commands[ALPHAOS_MAX_ISR80H_COMMANDS];
 extern void idt_load(struct idtr_desc *ptr);
 extern void int21h();
 extern void no_interrupt();
+extern void isr80h_wrapper();
 
 void int21h_handler()
 {
@@ -50,13 +52,46 @@ void idt_init()
 
     idt_set(0, idt_zero);
     idt_set(0x21, int21h);
+    idt_set(0x80, isr80h_wrapper);
 
     // Load the IDT
     idt_load(&idtr_descriptor);
 }
 
-void isr80h_handle_command(int command, struct interrupt_frame *frame)
-{}
+void isr80h_register_command(int command_id, ISR80H_COMMAND command)
+{
+    if (command_id <= 0 || command_id >= ALPHAOS_MAX_ISR80H_COMMANDS) {
+        // Invalid command
+        panic("isr80h_register_command(): Invalid command ID, index out of bounds\n");
+    }
+
+    if (isr80h_commands[command_id]) {
+        // Command already registered
+        panic("isr80h_register_command(): Command already registered\n");
+    }
+
+    isr80h_commands[command_id] = command;
+}
+
+void *isr80h_handle_command(int command, struct interrupt_frame *frame)
+{
+    void *res = 0;
+
+    if (command <= 0 || command >= ALPHAOS_MAX_ISR80H_COMMANDS) {
+        // Invalid command
+        return 0;
+    }
+
+    ISR80H_COMMAND command_func = isr80h_commands[command];
+    if (!command_func) {
+        // Command not implemented
+        return 0;
+    }
+
+    res = command_func(frame);
+
+    return res;
+}
 
 void *isr80h_handler(int command, struct interrupt_frame *frame)
 {
