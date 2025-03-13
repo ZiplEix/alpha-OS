0b9db7190295466caba314f4d90bd6bbe9caeba0
ZiplEix <ZiplEix@users.noreply.github.com>
2025-03-13 16:38:11 +0900

    feat(isr80h): add system call for keyboard input retrieval
    - Implemented `isr80h_command2_getkey()` to retrieve a key from the keyboard buffer.
    - Registered `SYSTEM_COMMAND2_GETKEY` in `isr80h_register_commands()`.
    - Updated `io.h` and `io.c` to include the new system call.
    - Modified `blank.asm` to use the new `getkey` system call before printing a message.
    
    This change allows user programs to wait for and retrieve keyboard input via system calls.

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -5,12 +5,21 @@ section .asm
 global _start
 
 _start:
+    call    getkey
     push    message
     mov     eax, 1 ; command print
     int     0x80
     add     esp, 4
 
     jmp $
+
+; wait for a key to be pressed, return the key in eax
+getkey:
+    mov     eax, 2 ; command getkey
+    int     0x80
+    cmp     eax, 0x00
+    je      getkey
+    ret
 
 section .data
 message: db 'Talking with the kernel !', 0
--- src/isr80h/io.c
+++ src/isr80h/io.c
@@ -1,7 +1,7 @@
 #include "io.h"
 #include "task/task.h"
 #include "kernel.h"
-
+#include "keyboard/keyboard.h"
 
 void *isr80h_command1_print(struct interrupt_frame *frame)
 {
@@ -12,4 +12,10 @@ void *isr80h_command1_print(struct interrupt_frame *frame)
     print(buf);
 
     return 0;
+}
+
+void *isr80h_command2_getkey(struct interrupt_frame *frame)
+{
+    char c = keyboard_pop();
+    return (void *)((int)c);
 }
