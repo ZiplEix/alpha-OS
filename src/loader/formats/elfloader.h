c74a28c69d1134ae69f31f4601afd608f5c23b10
ZiplEix <ZiplEix@users.noreply.github.com>
2025-03-14 14:51:56 +0900

    loader: add ELF loader implementation
    Introduce an ELF loader to handle loading and validating ELF binaries.
    - Implement `elf_get_entry_ptr` and `elf_get_entry` in `elf.c` for retrieving entry points.
    - Add ELF file validation and parsing functions in `elfloader.c`, including checks for signature, class, encoding, and program headers.
    - Define the `elf_file` struct in `elfloader.h` to manage ELF binary metadata, including memory layout and addresses.
    
    This lays the foundation for executing ELF binaries within the system.

---
+++ src/loader/formats/elfloader.h
@@ -0,0 +1,31 @@
+#ifndef ELF_LOADER_H
+#define ELF_LOADER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "elf.h"
+#include "config.h"
+
+struct elf_file {
+    char filename[ALPHAOS_MAX_PATH_LENGTH];
+
+    int in_memory_size;
+
+    // The physical memory address that this elf file os loaded at
+    void *elf_memory;
+
+    // The virtual base address of thes binary
+    void *virtual_base_address;
+
+    // The ending virtual address
+    void *virtual_end_address;
+
+    // The physical base address of the binary
+    void *physical_base_address;
+
+    // The physical end address of the binary
+    void *physical_end_address;
+};
+
+# endif // ELF_LOADER_H
