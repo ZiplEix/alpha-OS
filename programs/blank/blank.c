4327659d65ff7e9aab249ece83429f1fc11b5fb1
ZiplEix <ZiplEix@users.noreply.github.com>
2025-03-20 00:35:06 +0900

    stdio: add printf example and use of putchar and itoa
    This commit demonstrates the usage of the newly implemented `printf`, `putchar`, and `itoa` functions in the `blank.c` program. It includes a simple example to print a formatted string, an integer, and a character using `printf`, and adds a demonstration of `itoa` for integer to string conversion and `putchar` for character printing.
    
    Changes:
    - Added a `printf` example with formatted output to `blank.c`.
    - Included usage of `itoa` and `putchar` in `blank.c`.
    - Allocated and freed memory to test memory management.
    
    This commit demonstrates basic usage of the `stdio` and `stdlib` functions.

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,11 +1,16 @@
 #include "alphaos.h"
 #include "stdlib.h"
+#include "stdio.h"
 
 int main(int argc, char const *argv[])
 {
+    printf("Hello, World!, a number : %d another form of integer %i\n", 98, -89);
+
     print("Hello, World!\n");
 
     print(itoa(45678));
+
+    putchar('Z');
 
     void *ptr = malloc(512);
     free(ptr);
